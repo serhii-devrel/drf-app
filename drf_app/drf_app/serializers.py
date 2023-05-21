5302afa6221e6042c76393b16c4777333c5cba24
Serhii <sfrok99@gmail.com>
2023-05-21 15:55:44 +0300

    add support for queries

--- drf_app/drf_app/serializers.py
+++ drf_app/drf_app/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 
-from .models import Book, Author
+from .models import Book, Author, Category
 
-class BookSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field="author_full_name",
-        queryset = Author.objects.all()
-    )
 
-    class Meta:
-        model = Book
-        fields = ('title', 'author')
+class BookSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=128)
+    content = serializers.CharField(max_length=128)
+    time_create = serializers.DateTimeField()
+    time_update = serializers.DateTimeField()
+    is_published = serializers.BooleanField()
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
--- drf_app/drf_app/views.py
+++ drf_app/drf_app/views.py
@@ -1,8 +1,19 @@
-from rest_framework import generics
+from http import HTTPStatus
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from .models import Book
+from .models import Book, Author
 from .serializers import BookSerializer
 
-class BooksAPIView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BooksAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        books_serializer = BookSerializer(books, many=True)
+        return Response(books_serializer.data, status=HTTPStatus.OK)
+
+    def post(self, request):
+        books_serializer = BookSerializer(data=request.data)
+        if books_serializer.is_valid():
+            books_serializer.save()
+            return Response(books_serializer.data, status=HTTPStatus.CREATED)
+        return Response(books_serializer.errors, status=HTTPStatus.BAD_REQUEST)
